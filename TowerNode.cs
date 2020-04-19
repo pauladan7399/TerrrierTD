3a58795b48d4e671f557c09771104dfd7cf8fcba
ZimLim <36522291+ZimLim@users.noreply.github.com>
2020-04-19 14:09:29 -0400

    Add files via upload

---
+++ TowerNode.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerNode : MonoBehaviour
+{
+    public GameObject TowerMenuUI;
+    public Color hoverColor;
+    public Vector3 positionOffset;
+    private GameObject turret;
+
+    private Renderer rend;
+    private Color StartColor;
+
+    void Start () 
+        {
+            rend = GetComponent<Renderer>();
+            StartColor = rend.material.color;
+            TowerMenuUI.SetActive(false);
+
+        }
+
+    void OnMouseDown ()
+    {
+
+        TowerMenuUI.SetActive(true);
+        if (turret != null)
+        {
+            Debug.Log("Can't Build Here!");
+            return;
+        }
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position +positionOffset, transform.rotation);
+        Destroy(gameObject);
+    }
+    
+   void OnMouseEnter ()
+   {
+       rend.material.color = hoverColor;
+   }
+
+   void OnMouseExit ()
+   {
+       rend.material.color = StartColor;
+   }
+
+}
---
+++ WaveSpawner.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform eaglePrefab;
+    public Transform beaverPrefab;
+    public Transform spawnPoint;
+    public float timeBetweenWaves = 2f; //time between waves, AFTER the wave has been cleared
+    private float countdown = 2f;
+    
+
+    //hazim's addition
+    public Text waveText;
+    //
+
+    private int waveNumber = 20; //Starting on high wave for testing
+
+    void Update () 
+    {
+        /*if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        } */
+
+        if (GameObject.FindWithTag("Runner") == null){
+
+            
+            countdown -= Time.deltaTime; //counts down when there are no enemies 
+            //eventually, this should trigger the tower selection phase, then count down when the player is doen choosing their towers
+            //Wait for player to click done (opposed to putting the player on a timer)
+        }
+        if (countdown <= 0f){
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+    }
+
+    IEnumerator SpawnWave () {
+        waveNumber++;
+        for (int i=0; i<waveNumber; i++) {
+            SpawnRunner();
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        //displayWave(waveNumber);
+        
+    }
+
+    void SpawnRunner () {
+        //displays the wave
+        displayWave(waveNumber);
+        if (waveNumber % 2 == 0)
+        {
+            Instantiate(beaverPrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+        else
+        {
+            Instantiate(eaglePrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+
+    //hazim's additional method
+    //unity says this error : Object reference on set to an instance of an object
+    //but the game can run pretty decently 
+    public void displayWave(int x)
+    {
+        string num = x.ToString();
+
+        waveText.text = "Wave " + num;
+    }
+}
