21f683b0993fe3b9e6f36263c9f3eb7da1e729b8
ZimLim <36522291+ZimLim@users.noreply.github.com>
2020-04-22 01:12:07 +0800

    Update TowerNode.cs

--- TerrierTDgame/Assets/Scripts/TowerNode.cs
+++ TerrierTDgame/Assets/Scripts/TowerNode.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+
+  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,7 +13,8 @@ public class TowerNode : MonoBehaviour
 
     private Renderer rend;
     private Color StartColor;
-
+    //private int check = 0;
+ 
     void Start () 
         {
             rend = GetComponent<Renderer>();
@@ -30,9 +32,14 @@ public class TowerNode : MonoBehaviour
             Debug.Log("Can't Build Here!");
             return;
         }
-        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
-        turret = (GameObject)Instantiate(turretToBuild, transform.position +positionOffset, transform.rotation);
-        Destroy(gameObject);
+
+       
+        
+        
+        //GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        //turret = (GameObject)Instantiate(turretToBuild, transform.position +positionOffset, transform.rotation);
+        //Destroy(gameObject);
+        //TowerMenuUI.SetActive(false);
     }
     
    void OnMouseEnter ()
@@ -45,4 +52,32 @@ public class TowerNode : MonoBehaviour
        rend.material.color = StartColor;
    }
 
+   public void pho()
+    {
+        
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+        Destroy(gameObject);
+        TowerMenuUI.SetActive(false);
+    }
+
+    public void PE()
+    {
+        //check = 1;
+    }
+
+    public void citGo()
+    {
+        //check = 1;
+    }
+
+    public void warren()
+    {
+        //check = 1;
+    }
+
+    public void fenway()
+    {
+        //check = 1;
+    }
 }
