e04591bd941283daa7d11932eea9ccbc939f98c4
pauladan7399 <pauladan@bu.edu>
2020-04-24 01:26:02 -0400

    Added GameOver Screen and Game restarts when game is over

--- TerrierTDgame/Assets/Scripts/GameOver.cs
+++ TerrierTDgame/Assets/Scripts/GameOver.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
     private bool gameEnded = false;
+    public float restartDelay = 5f;
+    public SpriteRenderer sprite;
 
     // Update is called once per frame
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>(); //accessing SpriteRenderer
+        sprite.color = new Color(1, 1, 1, 0);
+    }
+    
     void Update()
     {
         if (gameEnded == true)
@@ -22,5 +31,11 @@ public class GameOver : MonoBehaviour
     void EndGame()
     {
         Debug.Log("You failed college!");
+        sprite.color = new Color(1, 1, 1, 1);
+        Invoke("Restart", restartDelay);
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
